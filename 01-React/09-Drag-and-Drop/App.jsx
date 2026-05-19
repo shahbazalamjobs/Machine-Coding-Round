bcaf523ca0419987715d3cd5d5ae77dbad3862b8
shahbazalamjobs <shahbazalamjobs@gmail.com>
2026-05-19 16:43:26 +0530

    drag and drop simpler logic update

--- 01-React/09-Drag-and-Drop/App.jsx
+++ 01-React/09-Drag-and-Drop/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
-import DndComponent from "./DndComponent";
-import  "./App.css"
+import DragAndDrop from "./DragAndDrop";
+const initialData = {
+  Todo: [
+    "Design UI mockups",
+    "Set up project repository",
+    "Write unit test",
+    "Integrate payment gateway",
+  ],
+  "In Progress": ["Develop authentication flow", "Implement responsive design"],
+  Completed: [
+    "Set up CI/CD pipeline",
+    "Conduct code reviews",
+    "Deploy initial version to staging",
+  ],
+};
 
-function App() {
-  return (
-    <div className="App">
-      <DndComponent />
-    </div>
-  );
+export default function App() {
+  return <DragAndDrop initialState={initialData} />;
 }
-
-export default App;
